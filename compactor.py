a9ade56aa96b1625a8b2c672d71ecfac382e35ed
EmmanuelEngineer <e.coppola37@studenti.unisa.it>
2022-05-02 18:31:22 +0200

    Primo Push post aggregazione

---
+++ compactor.py
@@ -0,0 +1,21 @@
+import csv
+
+import pandas
+import pandas as pd
+
+# Classe DataFrame csvFile
+csvPsy = pd.read_csv('Dataset/Youtube01-Psy.csv')
+print(csvPsy)
+
+csvKatyPerry = pd.read_csv('Dataset/Youtube02-KatyPerry.csv')
+print(csvKatyPerry)
+csvLMFAO = pd.read_csv('Dataset/Youtube03-LMFAO.csv')
+csvEminem = pd.read_csv('Dataset/Youtube04-Eminem.csv')
+csvShakira = pd.read_csv('Dataset/Youtube05-Shakira.csv')
+#Array che contiene tutti i csv
+cvsses = [csvPsy, csvKatyPerry, csvEminem, csvLMFAO, csvShakira]
+#Concatenazione di tutti i csv
+bigCvs = pd.concat(cvsses)
+print(bigCvs)
+#Scrittura su file csv
+bigCvs.to_csv('CompleteYoutubeDataset.csv')

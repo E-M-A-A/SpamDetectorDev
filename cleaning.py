733803c06eb3a48afa75a5d1caf7417d41a8853b
Muriel <m.rossi67@studenti.unisa.it>
2022-05-04 14:42:17 +0200

    Completata regex per l'eliminazione della punteggiatura e quella per la conversione da Uppercase a lowercase.

--- cleaning.py
+++ cleaning.py
@@ -5,16 +5,38 @@ import re
 clean_doc = pd.read_csv('Dataset/CompleteYoutubeDataset.csv')
 
 
-def Ciao(ciao):
+def spaces_remover(ciao):  #questa funzione rimuove spazi consecutivi > 1 dal dataset
     ciao = re.sub("\s+", " ", ciao)
     return ciao
 
 
-clean_doc['CONTENT'] = clean_doc['CONTENT'].apply(Ciao)
-pipo = clean_doc['CONTENT']
+clean_doc['CONTENT'] = clean_doc['CONTENT'].apply(spaces_remover)
+clean = clean_doc['CONTENT']
 
-for lines in pipo:
+
+def punt_remover(ciao): #questa funzione rimuove la punteggiatura dal dataset
+    ciao = re.sub("[^-9A-Za-z]", "", ciao)
+    return ciao
+
+
+clean_doc['CONTENT'] = clean_doc['CONTENT'].apply(punt_remover)
+clean = clean_doc['CONTENT']
+
+
+def lower_converter(ciao): #questa funzione converte le lettere maiuscole in minuscole
+    return ciao.lower()
+
+
+clean_doc['CONTENT'] = clean_doc['CONTENT'].apply(lower_converter)
+clean = clean_doc['CONTENT']
+
+
+for lines in clean:
     print(lines)
 
+
+
 clean_doc.to_csv('Dataset/DatasetNoSpace.csv')
+
+
 

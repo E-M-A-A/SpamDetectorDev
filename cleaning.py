dfaf6b20d4818cfdc1993f5fbd6c4369b23b679f
Muriel <m.rossi67@studenti.unisa.it>
2022-05-04 11:10:17 +0200

    Completata regex per l'eliminazione degli spazi.

--- cleaning.py
+++ cleaning.py
@@ -1,4 +1,20 @@
 import pandas as pd
 import nltk as nl
-pd.read_csv('Dataset/CompleteYoutubeDataset.csv')
+import re
+
+clean_doc = pd.read_csv('Dataset/CompleteYoutubeDataset.csv')
+
+
+def Ciao(ciao):
+    ciao = re.sub("\s+", " ", ciao)
+    return ciao
+
+
+clean_doc['CONTENT'] = clean_doc['CONTENT'].apply(Ciao)
+pipo = clean_doc['CONTENT']
+
+for lines in pipo:
+    print(lines)
+
+clean_doc.to_csv('Dataset/DatasetNoSpace.csv')
 

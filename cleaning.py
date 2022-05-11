a7c8280e9cf2c5241975394da180e8529c2458bf
EmmanuelEngineer <e.coppola37@studenti.unisa.it>
2022-05-11 18:19:02 +0200

    Test word correction

--- cleaning.py
+++ cleaning.py
@@ -2,6 +2,9 @@ import nltk
 import pandas as pd
 import re
 from nltk.tokenize import TweetTokenizer
+from nltk.corpus import words
+from nltk.metrics.distance  import edit_distance
+from pattern.en import spelling
 
 clean_doc = pd.read_csv('Dataset/CompleteYoutubeDataset.csv')
 
@@ -57,10 +60,52 @@ def spaces_remover(lines):  #questa funzione rimuove spazi consecutivi > 1 dal d
 
 clean_doc['CONTENT'] = clean_doc['CONTENT'].apply(spaces_remover)
 clean = clean_doc['CONTENT']
+nltk.download('words')
+correct_words = words.words()
+took = TweetTokenizer()
+
+
+def reduce_lengthening(text):
+    pattern = re.compile(r"(.)\1{2,}")
+    return pattern.sub(r"\1\1", text)
+
+
+def correct_words(text):
+    line = took.tokenize(text)
+    for word in line:
+        print("checking", word)
+        word = reduce_lengthening(word)
+        word = spelling(word)
+        print("correcting", word)
+
+    return " ".join(line)
+
+
+clean_doc['CONTENT'] = clean_doc['CONTENT'].apply(correct_words)
+clean = clean_doc['CONTENT']
+
+
+"""
+def word_corrector(x):  #questa funzione rimuove spazi consecutivi > 1 dal dataset
+    x = took.tokenize(x)
+    for y in x:
+        word_correction(y)
+        print("corrected", y)
+    return " ".join(x)
+
+
+def word_correction(y):
+    print("checking", y)
+    if y in correct_words:
+        return y
+    temp = [(edit_distance(y, w), w) for w in correct_words if w[0] == y[0]]
+    return sorted(temp, key=lambda val: val[0])[0][1]
+
+clean_doc['CONTENT'] = clean_doc['CONTENT'].apply(word_corrector)
+clean = clean_doc['CONTENT']"""
 
 
 stopwords = nltk.corpus.stopwords.words('english')
-took = TweetTokenizer()
 
 
 def stopwords_remover(x): #funzione per la rimozione delle stopwords
@@ -100,15 +145,11 @@ def lemmatizer(x): #funzione per la rimozione delle stopwords
 clean_doc['CONTENT'] = clean_doc['CONTENT'].apply(lemmatizer)
 clean = clean_doc['CONTENT']
 
-
-
-
-
-
+    
 for lines in clean:
     print(lines)
 
-clean_doc.to_csv('Dataset/DatasetNoSpace.csv')
+clean_doc.to_csv('Dataset/DatasetClean.csv')
 
 
 
